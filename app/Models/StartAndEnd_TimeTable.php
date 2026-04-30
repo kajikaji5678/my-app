4f8a25382fabce36e4179a80ce240c2c95ae76ea
福田舵斗 <adgjmfk@gmail.com>
2026-04-30 23:50:32 +0900

    出勤退勤時間のモデル作成

---
+++ app/Models/StartAndEnd_TimeTable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StartAndEnd_TimeTable extends Model
+{
+    use HasFactory;
+}
