dabbf065d19efbbbeae08161d1914ccd36c64df5
福田舵斗 <adgjmfk@gmail.com>
2026-05-01 22:56:24 +0900

    ヘルパー入れた

--- app/Http/Controllers/WorkTimeController.php
+++ app/Http/Controllers/WorkTimeController.php
@@ -4,20 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Carbon\Carbon;
-use App\Models\start_and_end_time;
+use App\Models\StartAndEndTime;
 
 class WorkTimeController extends Controller
 {
     public function go() {
-        $start = new start_and_end_time();
+        $start = new StartAndEndTime();
         $start->start_time = now();
         $start->save();
         return redirect('main')->with('message', '出勤ありがとう');
     }
 
     public function end() {
-        $end = Carbon::now();
-        session(['end_time' => $end]);
+        $end = StartAndEndTime::latest()->first();
+
+        if ($end) {
+            $end->end_time = Carbon::now();
+            $end->save();
+        }
         return redirect('main');
     }
 }
--- app/Models/StartAndEndTime.php
+++ app/Models/StartAndEndTime.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class start_and_end_time extends Model
+class StartAndEndTime extends Model
 {
     use HasFactory;
     protected $fillable = ['start_time', 'end_time'];
