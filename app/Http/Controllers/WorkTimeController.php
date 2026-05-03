1ee0e7476364821cfbdb64aa9e5888b752dbeacb
福田舵斗 <adgjmfk@gmail.com>
2026-05-04 01:26:20 +0900

    コントローラーにてidの情報の組み込み

--- app/Http/Controllers/WorkTimeController.php
+++ app/Http/Controllers/WorkTimeController.php
@@ -5,21 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Models\StartAndEndTime;
+use Illuminate\Support\Facades\Auth; // 認証機能クラスを使用
 
 class WorkTimeController extends Controller
 {
     public function go() {
         $start = new StartAndEndTime();
         $start->start_time = now();
+        $start->user_id = Auth::id(); // その人のidをぶち込む
         $start->save();
         return redirect('main')->with('message', '出勤ありがとう');
     }
 
     public function end() {
-        $end = StartAndEndTime::latest()->first();
+        $end = Auth::user()->works()->latest()->first(); 
+        // ログインユーザーの勤怠一覧を新しい順に並べて一件取得
 
         if ($end) {
-            $end->end_time = Carbon::now();
+            $end->end_time = Carbon::now(); // end_timeはnullなんで追加
             $end->save();
         }
         return redirect('main');
