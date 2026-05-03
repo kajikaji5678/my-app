d0bc43d465a8befc29e2c0eb6ca8db2e34d359d6
福田舵斗 <adgjmfk@gmail.com>
2026-05-04 00:59:07 +0900

    認証の基礎インストール

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\WorkTimeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,10 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/main', function () {
-    return view('main');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::post('/main/post', [WorkTimeController::class, 'go']);
-Route::post('/main/end', [WorkTimeController::class, 'end']);
-Route::get('/main/list', [WorkTimeController::class, 'get']);
+require __DIR__.'/auth.php';
