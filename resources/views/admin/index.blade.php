1e37297256dca0853c102b009b20e3c48540b92a
福田舵斗 <adgjmfk@gmail.com>
2026-05-04 13:28:10 +0900

    css追加

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html lang="Ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 </head>
+
 <body>
-    <a href={{ "/admin/working" }}>出勤一覧ページ</a>
-    <a href="{{ "/admin/salary_total" }}">支出一覧ページ</a>
+    <div class="admin_top">
+        <a href={{ "/admin/working" }}>出勤一覧ページ</a>
+        <a href="{{ " /admin/salary_total" }}">支出一覧ページ</a>
+    </div>
 </body>
+
 </html>
