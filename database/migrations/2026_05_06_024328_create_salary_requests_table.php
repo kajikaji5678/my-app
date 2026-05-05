d51be5be5e1deb8ac9127efac6eb79f99d55907c
福田舵斗 <adgjmfk@gmail.com>
2026-05-06 02:49:22 +0900

    給与申請マイグレ

---
+++ database/migrations/2026_05_06_024328_create_salary_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salary_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->integer('before_salary')->default(1000);
+            $table->integer('after_salary');
+            $table->text('reason');
+            $table->string('status')->default('pending');
+            $table->unsignedBigInteger('approved_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salary_requests');
+    }
+};
