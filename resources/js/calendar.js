8985452dbaefd229cd5cd90fc641292db33b909c
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-05-05 14:49:38 +0900

    カレンダーに予定を入れる機能を追加

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,12 +1,23 @@
 import { Calendar } from '@fullcalendar/core'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 document.addEventListener('DOMContentLoaded', () => {
     const el = document.getElementById('calendar');
     const calendar = new Calendar(el, {
         locale: 'ja',
-        plugins: [dayGridPlugin],
-        initialView: 'dayGridMonth'
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth', // 月別表示
+        dateClick: function(info) {
+            const title = prompt('予定を入力');
+            if (title) {
+                calendar.addEvent({
+                    title: title,
+                    start: info.dateStr,
+                    color: 'green'
+                });
+            }
+        }
     });
 
     calendar.render();
