913562a1b8317282bb1b765ecde8b45193aa08a2
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-05-14 14:17:43 +0900

    書き直し

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,6 +1,7 @@
 import { Calendar } from '@fullcalendar/core'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import { start } from 'alpinejs';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -32,29 +33,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
         dateClick: function(info) {
             const title = prompt('予定を入力');
-            if (title) {
-                // POST送信
-                fetch('/main/schedules',{
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-                    },
-                    body: JSON.stringify({
-                        title: title,
-                        date: info.dateStr
-                    })
-                }).then(response => response.json())
-                .then(date => {
-                    calendar.addEvent({
-                        title: date.title,
-                        start: date.date,
-                        color: 'blue'
-                    });
-                }).catch(error => {
-                    console.error('Error:', error);
+            if (!title) return;
+            const startTime = prompt('開始時間');
+            if (!startTime) return;
+            const endTime = prompt('終了時間');
+            if (!endTime) return;
+
+            fetch('/main/schedules', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                },
+                body: JSON.stringify({
+                    title: title,
+                    date: info.dateStr,
+                    startTime: startTime,
+                    end_time: endTime,
+                    status: 'pending'
+                })
+            })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(res.status);
+                }
+                return res.json();
+            })
+            .then(data => {
+                calendar.addEvent({
+                    title: `${data.title} ${data.start_time} ~ ${data.end_time}`,
+                    start: data.date,
+                    color: 'orange'
                 });
-            }
+            })
+            .catch(error => {
+                console.error(error);
+            });
         }
     });
 
