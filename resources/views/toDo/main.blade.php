f2f5d7e9db0aaffe7cca23ef57e4762daa4f62ad
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-05-23 02:10:56 +0900

    26/5/23 2 メインフロントHTML作成

--- resources/views/toDo/main.blade.php
+++ resources/views/toDo/main.blade.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/main2.css') }}">
 </head>
+
 <body>
-    
+    <header>
+        <ul>
+            <li>ダッシュボード</li>
+            <li>プロジェクト</li>
+            <li>お知らせ</li>
+            <li>メンバー招待</li>
+        </ul>
+    </header>
+
+    <main>
+        <aside>
+
+        </aside>
+    </main>
 </body>
+
 </html>
