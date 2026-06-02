17854aa7d854196e844345dd44065ebc91b379b3
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-06-02 16:53:10 +0900

    note3.phpさくせい

---
+++ note3.php
@@ -0,0 +1,41 @@
+<?php
+
+// todo 6/2 コンストラクタDI
+
+interface interfaceGreet
+{
+    public function hello();
+}
+
+class Greet implements interfaceGreet
+{
+    public function hello()
+    {
+        return 'こんにちは';
+    }
+}
+
+class EnglishGreet implements interfaceGreet {
+    public function hello() {
+        return 'hello';
+    }
+}
+
+class User2
+{
+    private $greet;
+
+    public function __construct(interfaceGreet $greet)
+    {
+        $this->greet = $greet;
+    }
+
+    public function greet()
+    {
+        echo $this->greet->hello();
+    }
+}
+
+$greet = new Greet;
+$user = new User2(new EnglishGreet());
+$user->greet();
