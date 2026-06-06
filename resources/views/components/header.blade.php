b637863153b16d36dcc24b3dc0351464067aa35e
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-06-07 00:21:13 +0900

    お知らせモーダル基盤

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,4 @@
-@props(['notifications']);
-
+@props(['notifications'])
 <header>
     <ul>
         <li>
@@ -8,8 +7,18 @@
         <li>
             <a href="">プロジェクト</a>
         </li>
-        <li id="{{ isset($notifications) ? "notification" : "" }}" >
+        <li id="{{ isset($notifications) ? 'notification' : '' }}">
             <a href="">お知らせ</a>
+            <div class="notification-modal">
+                <ul>
+                    <li class="notification-li">
+                        という件名にてアサインされています！
+                    </li>
+                    <li class="notification-li">
+                        という件名にてアサインされています！
+                    </li>
+                </ul>
+            </div>
         </li>
     </ul>
 </header>
