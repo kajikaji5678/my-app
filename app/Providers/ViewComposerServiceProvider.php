966e4b3273933a2ae0658c40b6529cd99dc9be88
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-06-07 18:34:18 +0900

    プロバイダー設定バグ

--- app/Providers/ViewComposerServiceProvider.php
+++ app/Providers/ViewComposerServiceProvider.php
@@ -10,21 +10,18 @@ class ViewComposerServiceProvider extends ServiceProvider
     /**
      * Register services.
      */
-    public function register(): void
-    {
-        View::composer('*', function($view) {
-            $notifications = auth()->user()->unreadNotifications;
-            $notificationCout = $notifications->count();
-            $view->with('notifications',$notifications);
-            $view->with('notificationCount', $notificationCout);
-        });
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap services.
      */
     public function boot(): void
     {
-        //
+        View::composer('*', function ($view) {
+            $notifications = auth()->user()->unreadNotifications;
+            $notificationCout = $notifications->count();
+            $view->with('notifications', $notifications);
+            $view->with('notificationCount', $notificationCout);
+        });
     }
 }
