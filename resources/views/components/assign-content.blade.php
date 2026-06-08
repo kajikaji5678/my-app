2dc252ebd65c6855b5b4ab556ada9a81826b0888
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-06-08 09:44:06 +0900

    目標

---
+++ resources/views/components/assign-content.blade.php
@@ -0,0 +1,28 @@
+@props(['assigns'])
+
+@foreach ($assigns as $assign)
+    <div class="assign-content">
+        <div class="assign-content-block">
+            <p class="assign-content-title">アサイン名</p>
+            <p class="assign-content-p">{{ $assign->assign_name }}</p>
+        </div>
+        <div class="assign-content-block">
+            <p class="assign-content-title">内容</p>
+            <p class="assign-content-p">{{ $assign->assign_content }}</p>
+        </div>
+        <div class="assign-content-block">
+            <p class="assign-content-title">募集開始時刻</p>
+            <p class="assign-content-p">{{ $assign->start_time }}</p>
+        </div>
+        <div class="assign-content-block">
+            <p class="assign-content-title">募集終了時刻</p>
+            <p class="assign-content-p">2009/01/01</p>
+        </div>
+        <div class="assign-content-block">
+            <p class="assign-content-title">対象者</p>
+            <p class="assign-content-p">福田舵斗</p>
+        </div>
+    </div>
+@endforeach
+
+{{-- コミット目標 --}}
--- resources/views/toDo/assign.blade.php
+++ resources/views/toDo/assign.blade.php
@@ -27,30 +27,11 @@
                         アサイン作成
                     </div>
                 </a>
-                <div class="assign-main">
-                    <div class="assign-content">
-                        <div class="assign-content-block">
-                            <p class="assign-content-title">アサイン名</p>
-                            <p class="assign-content-p">テスト</p>
-                        </div>
-                        <div class="assign-content-block">
-                            <p class="assign-content-title">内容</p>
-                            <p class="assign-content-p">あああああああ</p>
-                        </div>
-                        <div class="assign-content-block">
-                            <p class="assign-content-title">募集開始時刻</p>
-                            <p class="assign-content-p">2000/01/01</p>
-                        </div>
-                        <div class="assign-content-block">
-                            <p class="assign-content-title">募集終了時刻</p>
-                            <p class="assign-content-p">2009/01/01</p>
-                        </div>
-                        <div class="assign-content-block">
-                            <p class="assign-content-title">対象者</p>
-                            <p class="assign-content-p">福田舵斗</p>
-                        </div>
+                @if (isset($assigns))
+                    <div class="assign-main">
+                        <x-assign-content :assigns="$assigns"/>
                     </div>
-                </div>
+                @endif
             </div>
         </main>
     </div>
