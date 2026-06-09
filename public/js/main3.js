01dd278067f783ee6d2a8818c322bb6e9cfeffd2
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-06-09 13:29:51 +0900

    ファイル整理

--- public/js/main3.js
+++ public/js/main3.js
@@ -4,10 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
     let statusId = null;
 
     const modal = document.getElementById('modal');
+    const openButtons = document.querySelectorAll('.dynamic_box_top2');
+    const tasks = document.querySelectorAll('.dynamic_box_content')
+    const assignButton = document.querySelector('.assign_button');
 
     function render() {
         modal.dataset.mode = mode;
         document.getElementById('task_id').value = taskId;
         document.getElementById('status_id').value = statusId;
     }
+
+    openButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            statusId = btn.dataset.statusId;
+            mode = '1';
+            render();
+        });
+    });
+
+    tasks.forEach(task => {
+        task.addEventListener('click', () => {
+            taskId = task.dataset.taskId;
+            mode = '2';
+            render();
+        });
+    });
+    assignButton.addEventListener('click', () => {
+        modal.dataset.mode = '3';
+    });
 });
