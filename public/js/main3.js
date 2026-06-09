e4731f0acc3306eb633e72adabfb68b6c01c19d0
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-06-09 15:37:35 +0900

    API練習

--- public/js/main3.js
+++ public/js/main3.js
@@ -6,15 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const modal = document.getElementById('modal');
     const openButtons = document.querySelectorAll('.dynamic_box_top2');
-    const tasks = document.querySelectorAll('.dynamic_box_content')
+    const tasks = document.querySelectorAll('.dynamic_box_content');
     const assignButton = document.querySelector('.assign_button');
+    const modalClose = document.querySelector('.modal_close');
 
     // DOM更新をここで集約している
     function render() {
         modal.dataset.mode = mode;
-        document.getElementById('task_id').value = taskId;
-        document.getElementById('status_id').value = statusId;
+        document.getElementById('task_id').value = taskId ?? '';
+        document.getElementById('status_id').value = statusId ?? '';
     }
+
+    modalClose.addEventListener('click', () => {
+        mode = '0'
+        render();
+    });
 
     openButtons.forEach(btn => {
         btn.addEventListener('click', () => {
@@ -25,8 +31,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     tasks.forEach(task => {
-        task.addEventListener('click', () => {
+        task.addEventListener('click', async () => {
             taskId = task.dataset.taskId;
+            const response = await fetch(`/api/tasks/${taskId}`);
+
+            if (!response.ok) {
+                throw new Error(`HTTP Error: ${response.status}`);
+            }
+            const taskData = await response.json();
+            console.log(taskData);
             mode = '2';
             render();
         });
