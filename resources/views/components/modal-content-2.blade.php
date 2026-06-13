73e811a47352afdc44132eb14c6da936ba9c5972
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-06-13 14:31:02 +0900

    モーダル-タスク詳細-CSS

--- resources/views/components/modal-content-2.blade.php
+++ resources/views/components/modal-content-2.blade.php
@@ -1,19 +1,34 @@
 @props(['types', 'milestones', 'categories', 'statuses'])
 <div class="modal_content_bottom_2" id="modal_content_bottom_2">
-    <div class="modal-form">
-        <form action="{{ route('board.status') }}" method="POST">
-            @csrf
-            <input type="hidden" id="task_id" name="task_id">
+    <div class="modal-left">
+        <div class="modal-info">
+            <p class="modal-info-title">タスク名</p>
+            <p>あああ</p>
+        </div>
+        <div class="modal-info">
+            <p class="modal-info-p">カテゴリー</p>
+        </div>
+        <div class="modal-info">
+            <p class="modal-info-p">タイプ</p>
+        </div>
+        <div class="modal-info">
+            <p class="modal-info-p">マイルストーン</p>
+        </div>
+        <div class="modal-info">
             <p class="modal_type">ステータス更新</p>
-            <select name="status_id" class="modal_form">
-                @foreach ($statuses as $status)
-                    <option value="{{ $status->id }}">
-                        {{ $status->status_name }}
-                    </option>
-                @endforeach
-            </select>
-            <button type="submit">送信</button>
-        </form>
+            <form action="{{ route('board.status') }}" method="POST" class="modal-form-2">
+                @csrf
+                <input type="hidden" id="task_id" name="task_id">
+                <select name="status_id" class="modal_form">
+                    @foreach ($statuses as $status)
+                        <option value="{{ $status->id }}">
+                            {{ $status->status_name }}
+                        </option>
+                    @endforeach
+                </select>
+                <button type="submit">送信</button>
+            </form>
+        </div>
     </div>
     <div class="modal-comment">
         <p class="comment-title">コメント一覧</p>
