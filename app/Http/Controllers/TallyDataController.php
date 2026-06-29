995e48f2d1f44be84a0d25ed836f7380e0d124b7
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-06-30 05:21:58 +0900

    コントローラーにてランキング修正

--- app/Http/Controllers/TallyDataController.php
+++ app/Http/Controllers/TallyDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Services\UserTaskProcessing;
 use Illuminate\Http\Request as HttpRequest;
 use Request;
@@ -9,6 +10,24 @@ use Request;
 class TallyDataController extends Controller
 {
     // ~ 以下 算出コンテナの利用 6/26
+    public function index() {
+        $users = User::all();
+        $calculation = new UserTaskProcessing();
+
+        $ranking = [];
+        foreach ($users as $user) {
+            $ranking[] = [
+                'name' => $user->name,
+                'rate' => $calculation->getWeeklyUser($user->id),
+            ];
+        }
+
+        $sortedRanking = collect($ranking)->sortByDesc('rate');
+
+        return view('toDo.graph', compact('sortedRanking'));
+    }
+
+
     public function test(HttpRequest $request)
     {
         $id = $request->user_id;
