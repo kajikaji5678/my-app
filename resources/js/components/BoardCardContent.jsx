672167bbab068504411e413d6ed6a086891e6042
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-07-23 16:52:44 +0900

    shadcn入手

--- resources/js/components/BoardCardContent.jsx
+++ resources/js/components/BoardCardContent.jsx
@@ -38,21 +38,3 @@ export default function BoardCardContet({
 
 
 
-{/* <div key={task.id}
-  className={`h-20 rounded border border-[#C5C5C5] mt-3 p-2 transition duration-300 cursor-pointer relative ${animation}`}
-  data-task-id={task.id}
-  data-status-id={task.status_id}>
-  <p className="py-1 px-2 text-xs text-white rounded-lg w-fit"
-    style={{ backgroundColor: task.type?.type_color }}>
-    {task.type?.type_name}
-  </p>
-  <p className="text-sm my-1">
-    {task.task_name}
-  </p>
-  <div className="flex justify-between">
-    <p className="text-xs text-[#ca0000]">
-      {task.created_at}
-    </p>
-  </div>
-</div> */}
-
