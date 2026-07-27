50436fdc654727b97d7a455e8679155ae4fa2059
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-07-27 15:16:57 +0900

    toDoBoard_modal_front_condiBranch

--- resources/js/components/TaskSheet/TaskLabel.tsx
+++ resources/js/components/TaskSheet/TaskLabel.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type row = {
   label: string;
   value: string | undefined;
@@ -7,12 +9,19 @@ type Props = {
   rows: row[];
 }
 
-export default function TaskLabel({rows} : Props) {
+export default function TaskLabel({ rows }: Props) {
+
+  const [edit, setEdit] = useState(false);
+
   return (
     <>
       <div className="w-full flex items-center justify-between px-4 py-3 border-b bg-gray-200">
         <h2 className="font-semibold">詳細</h2>
-        <button className="text-blue-600 text-sm hover:underline cursor-pointer">編集</button>
+        <button
+          className="text-blue-600 text-sm hover:underline cursor-pointer"
+          onClick={() => setEdit(!edit)}>
+          {edit ? "キャンセル" : "編集"}
+        </button>
       </div>
       <div className="grid grid-cols-[120px_1fr]">
         {rows.map((row: row) => (
@@ -21,7 +30,15 @@ export default function TaskLabel({rows} : Props) {
               {row.label}
             </p>
             <p className="font-semibold px-4 py-3">
-              {row.value}
+              {edit ? (
+                <input
+                value={row.value}
+                className="w-full border rounded px-2 py-1"/>
+              ) : (
+                <p className="font-semibold">
+                  {row.value}
+                </p>
+              )}
             </p>
           </>
         ))}
