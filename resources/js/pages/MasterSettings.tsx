c6f883e65039dac43292ecf61ddeb905be7bc445
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-08-21 13:40:34 +0900

    front: tsx型挿入

--- resources/js/pages/MasterSettings.tsx
+++ resources/js/pages/MasterSettings.tsx
@@ -1,3 +1,34 @@
+type MasterType = "category" | "type" | "status";
+
+type MasterItem = {
+  id: number,
+  name: string
+};
+
+const initialDate: Record<MasterType, MasterItem[]> = {
+  category: [
+    { id: 1, name: "開発" },
+    { id: 2, name: "デザイン" },
+    { id: 3, name: "バグ修正" }
+  ],
+  type: [
+    { id: 1, name: "機能修正" },
+    { id: 2, name: "修正" },
+    { id: 3, name: "調査" }
+  ],
+  status: [
+    { id: 1, name: "未着手" },
+    { id: 2, name: "進行中" },
+    { id: 3, name: "完了" }
+  ]
+};
+
+const titles: Record<MasterType, string> = {
+  category: "カテゴリー",
+  type: "タイプ",
+  status: "ステータス"
+}
+
 export default function MasterSettings() {
   return (
     <div>
